986d5e957ff6cf1fd091bc680845702e08049ebe
Mark Cui Pan <mpan5@wisc.edu>
2019-05-09 12:13:04 -0500

    Add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flashcard.create(
+  [
+    { term: 'Hobbit', definition: 'a member of an imaginary race similar to humans, of small size and with hairy feet, in stories by J. R. R. Tolkien' },
+    { term: 'Cellular Respiration', definition: 'C6H12O2 + O2 => H20 + CO2 + Energy' },
+    { term: 'Creativity', definition: 'the use of the imagination or original ideas, especially in the production of an artistic work' }
+  ]
+)
