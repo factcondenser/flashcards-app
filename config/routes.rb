c4ae555fb90f373f64456da237a06638b4e8f7d7
Mark Cui Pan <mpan5@wisc.edu>
2019-05-12 17:12:11 -0500

    Add code to register a serviceworker

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'flashcards#index'
   resources :flashcards
+
+  # Service Worker Routes
+  get '/service-worker.js' => 'service_worker#service_worker'
+  get '/manifest.json' => 'service_worker#manifest'
 end
